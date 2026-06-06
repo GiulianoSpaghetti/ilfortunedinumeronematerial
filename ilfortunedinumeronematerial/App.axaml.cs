c624d8d7af7d12e9d9abef20f0993d6aae33c124
Giulio Sorrentino <numerone@fastwebnet.it>
2026-06-06 05:02:17 +0200

    latest stable version

--- ilfortunedinumeronematerial/App.axaml.cs
+++ ilfortunedinumeronematerial/App.axaml.cs
@@ -3,11 +3,15 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using ilfortunedinumeronematerial.ViewModels;
 using ilfortunedinumeronematerial.Views;
+using System.ComponentModel;
 
 namespace ilfortunedinumeronematerial;
 
 public partial class App : Application
 {
+
+    public static int Millisecondi { get; private set; }
+    public static int Tentativi {get; private set; }
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -17,10 +21,14 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            Millisecondi = 1000;
+            Tentativi = 10;
             desktop.MainWindow = new MainWindow();
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
+            Millisecondi = 900;
+            Tentativi = 4;
             singleViewPlatform.MainView = new MainView();
         }
 
--- ilfortunedinumeronematerial/ViewModels/MainViewModel.cs
+++ ilfortunedinumeronematerial/ViewModels/MainViewModel.cs
@@ -1,9 +1,14 @@
-﻿using System.Net;
-using System.Reflection.PortableExecutable;
-using System;
-using Avalonia.Platform.Storage;
+﻿using Avalonia.Platform.Storage;
+using Avalonia.Threading;
+using Material.Styles.Controls;
 using ReactiveUI;
+using System;
+using System.Data;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Reflection.PortableExecutable;
+using System.Threading;
 using System.Windows.Input;
 
 namespace ilfortunedinumeronematerial.ViewModels;
@@ -18,6 +23,7 @@ public class MainViewModel : ViewModelBase
     private int id;
     private string _cookie = string.Empty;
     private bool _continua = true;
+    private String _status = string.Empty;
     public String Message
     {
         get => $"Per ottenere un doppione cliccare sul pulsante \"Ottieni un nuovo Biscotto\" per {max} volte.";
@@ -33,21 +39,24 @@ public class MainViewModel : ViewModelBase
         get => _continua;
         set => this.RaiseAndSetIfChanged(ref _continua, value);
     }
-    public ICommand Click { get; private set; }
+    public string Status
+    {
+        get => _status; set
+        {
+            this.RaiseAndSetIfChanged(ref _status, value);
+            SnackbarHost.Post(_status, null, DispatcherPriority.Normal);
+        }
+    }
+
+    public ReactiveUI.ReactiveCommand<System.Reactive.Unit, System.Reactive.Unit> Click {get; }
+
     public MainViewModel()
     {
         try
         {
-            conn = new("server=numeronesoft.ddns.net;user=guest;database=barzellette;port=3306");
-            conn.Open();
-            rnd = new();
-            cmd = new("SELECT MAX(ID) FROM Barzellette", conn);
-            reader = cmd.ExecuteReader();
-            reader.Read();
-            max = reader.GetInt32(0);
-            reader.Close();
+            connect(App.Tentativi);
             GetCookie();
-            Click=ReactiveCommand.Create(GetCookie);
+	    Click=ReactiveCommand.Create(GetCookie);
         }
         catch (Exception ex)
         {
@@ -70,8 +79,39 @@ public class MainViewModel : ViewModelBase
         }
         catch (Exception ex)
         {
-            Cookie = ex.Message;
-            Continua = false;
+            try
+            {
+                connect(App.Tentativi);
+                GetCookie();
+            }
+            catch (Exception ex2)
+            {
+                Status = ex.Message;
+                Continua = false;
+            }
+        }
+    }
+
+    private void connect(int tentative)
+    {
+        try
+        {
+            conn = new("server=numeronesoft.ddns.net;user=guest;database=barzellette;port=3306");
+            conn.Open();
+            rnd = new();
+            cmd = new("SELECT MAX(ID) FROM Barzellette", conn);
+            reader = cmd.ExecuteReader();
+            reader.Read();
+            max = reader.GetInt32(0);
+            reader.Close();
+        } catch (Exception ex) {
+            if (tentative < 1)
+                throw ex;
+            else
+            {
+                Thread.Sleep(App.Millisecondi);
+                connect(tentative - 1);
+            }
         }
     }
 }
